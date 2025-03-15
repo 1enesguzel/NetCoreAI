cd8de858e977a2ab24fc59f9f83044864061abd8
Enes Güzel <guzel.gz634@gmail.com>
2025-03-15 14:43:39 +0300

    Müşteri listesi oluşturuldu

---
+++ NetCoreAI.Project02_ApiConsumeAI/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAI.Project02_ApiConsumeAI.Dtos;
+using Newtonsoft.Json;
+
+namespace NetCoreAI.Project02_ApiConsumeAI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public CustomerController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> CreateCostumer()
+        {
+            var client = _clientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7107/api/Customers");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ NetCoreAI.Project02_ApiConsumeAI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreAI.Project02_ApiConsumeAI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
