29d2e5258ed981f5ecaf294d604b658a50faa613
Enes Güzel <guzel.gz634@gmail.com>
2025-03-15 15:51:22 +0300

    güncelle eklendi

--- NetCoreAI.Project02_ApiConsumeAI/Controllers/CustomerController.cs
+++ NetCoreAI.Project02_ApiConsumeAI/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetCoreAI.Project02_ApiConsumeAI.Dtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace NetCoreAI.Project02_ApiConsumeAI.Controllers
 {
@@ -13,7 +14,7 @@ namespace NetCoreAI.Project02_ApiConsumeAI.Controllers
             _clientFactory = clientFactory;
         }
 
-        public async Task<IActionResult> CreateCostumer()
+        public async Task<IActionResult> CustomerList()
         {
             var client = _clientFactory.CreateClient();
             var response = await client.GetAsync("https://localhost:7107/api/Customers");
@@ -22,6 +23,60 @@ namespace NetCoreAI.Project02_ApiConsumeAI.Controllers
                 var jsonData = await response.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer(CreateCustomerDto customerDto)
+        {
+            var client = _clientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(customerDto);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7107/api/Customers", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CustomerList");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var client = _clientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7107/api/Customers?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CustomerList");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateCustomer(int id)
+        {
+            var client = _clientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7107/api/Customers/GetCustomer?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdCustomerDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCustomer(UpdateCustomerDto updateCustomerDto)
+        {
+            var client = _clientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(updateCustomerDto);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7107/api/Customers", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CustomerList");
             }
             return View();
         }
